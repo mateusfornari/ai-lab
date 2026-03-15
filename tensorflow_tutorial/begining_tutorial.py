496426da10f2f5fb18fe1d35f4a9949d63418e3a
mateus <mateusfornari@gmail.com>
2026-03-15 14:07:14 -0300

    Changed begining_tutorial to use fashion dataset and display the image with predicted label.

--- tensorflow_tutorial/begining_tutorial.py
+++ tensorflow_tutorial/begining_tutorial.py
@@ -1,21 +1,78 @@
 import tensorflow as tf
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
 
-mnist = tf.keras.datasets.mnist
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
+    plt.imshow(img, cmap=plt.cm.binary)
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    predicted_label = predictions_array.argmax()
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
 
-model.fit(x_train, y_train, epochs=5)
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
 
-print(model.evaluate(x_test, y_test, verbose=2))
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+def main():
+    mnist = tf.keras.datasets.fashion_mnist
+
+    (x_train, y_train),(x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=10)
+
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+    print('Test loss:', test_loss)
+    print('Test accuracy:', test_acc)
+
+    predictions = model.predict(x_test)
+    print(predictions[0].argmax())
+
+    for i in range(1):
+        plt.figure(figsize=(6, 3))
+        plt.subplot(1, 2, 1)
+        plot_image(i, predictions, y_test, x_test)
+        plt.subplot(1, 2, 2)
+        plot_value_array(i, predictions, y_test)
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
